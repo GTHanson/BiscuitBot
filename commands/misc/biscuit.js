99ad860ba7c6bd601b3852f8a1470ebe2185adbf
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-09-23 00:39:32 -0700

    Create biscuit.js

---
+++ commands/misc/biscuit.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: "biscuit",
+	description: "Tell Biscuit something",
+
+	execute(message, args) {
+		message.channel.messages.fetch({ limit: 1 }).then(messages => {
+			let lastMessage = messages.first();
+			
+			if (!lastMessage.author.bot) {
+			  lastMessage.delete();
+			}
+		});
+		var messageText = "";
+		for(var i = 0; i < args.length; i++) {
+			messageText += args[i] + " ";
+		}
+		message.channel.send({ content: messageText});
+	},
+};
