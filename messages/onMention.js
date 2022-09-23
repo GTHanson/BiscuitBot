97852c41e891d58c08a0715d289fc1f24e748d92
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-09-23 01:02:07 -0700

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -1,14 +1,26 @@
 const cleverbot = require("cleverbot-free");
+const fs = require('fs');
 
 module.exports = {
     async execute(message) {
+        var messageContext = [];
+
         var messageText = message.content.substring(22);
 
-        
         console.log(messageText);
 
-		cleverbot(messageText).then(response => 
-            message.channel.send({ content: response})
-            );
+		cleverbot(messageText).then(response => message.channel.send({ content: response}));
+
+        fs.writeFile('cleverbot-memory.txt', messageText, err => {
+            if (err) {
+              console.error(err);
+            }
+        });
+
+        fs.writeFile('cleverbot-memory.txt', response, err => {
+            if (err) {
+              console.error(err);
+            }
+        });
 	},
 };
