ff6f63d4b0f8f803d6565b1f689b5ade9f950afb
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-09-29 20:19:21 -0700

    Update speak.js

--- interactions/slash/misc/speak.js
+++ interactions/slash/misc/speak.js
@@ -1,7 +1,7 @@
 const { MessageEmbed, Collection } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer } = require('@discordjs/voice');
 
 // Specifically for button interactions.
 const { MessageButton } = require('discord.js');
