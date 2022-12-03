fe8ce659788704483b538844467f150b129eab29
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-03 13:48:48 -0800

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -1,32 +1,31 @@
-const cleverbot = require("cleverbot-free");
-const fs = require('fs');
+const { OPENAI_SECRET_KEY } = require("../config.json");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: OPENAI_SECRET_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const Personalities = 
+{
+    def : "You are an energetic Golden Retriever who is my pet. Your name is \"Biscuit\"\n",
+    none : ""
+}
 
 module.exports = {
     async execute(message) {
+        var prompt =  Personalities.def + `Respond to the prompt \'${message.content.substring(22)}\'\n`;
+        if(message.content.includes("Remove Emotional Affect"))
+        {
+            prompt = Personalities.none + `\'${message.content.substring(message.content.indexOf("Remove Emotional Affect") + 25)}\'\n`;
+        }
+        (async () => {
+            const gptResponse = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: prompt,
+                temperature: 0.6,
+                max_tokens: 1024,
+              });
+            message.reply(`${gptResponse.data.choices[0].text}`);
 
-        var messageContext = fs.readFileSync('cleverbot-memory.txt').toString().split("\n");
-
-        var prompt = message.content.substring(22);
-
-        console.log(prompt);
-
-		cleverbot(prompt, messageContext).then(response => RespondToMessage(message, response, prompt));
+        })();
 	},
 };
-
-function RespondToMessage(message, response, prompt)
-{
-    message.channel.send({ content: response});
-    
-    fs.writeFile('cleverbot-memory.txt', prompt + "\n", { flag: 'a+' }, err => {
-        if (err) {
-          console.error(err);
-        }
-    });
-
-    fs.writeFile('cleverbot-memory.txt', response + "\n", { flag: 'a+' }, err => {
-        if (err) {
-          console.error(err);
-        }
-    });
-}
